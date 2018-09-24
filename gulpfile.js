57b0db59ce635636bc07d46ce4e6867e0b902839
Morgan Segura <morgansegura@gmail.com>
2018-09-24 15:11:54 -0700

    Add postcss and postcss libs

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+// gulp watch
+var gulp = require('gulp'),
+    processor = require('postcss-processor-order'),
+    postcss = require('gulp-postcss'),
+    cssImport = require('postcss-import'),
+    mixins = require('postcss-mixins'),
+    cssvars = require('postcss-simple-vars'),
+    nested = require('postcss-nested'),
+    colorFunctions = require('postcss-color-function'),
+    pixelsToRem = require('postcss-pixels-to-rem'),
+    cssnano = require('gulp-cssnano'),
+    cssNext = require('postcss-preset-env');
+
+gulp.task('styles', function () {
+    return gulp.src('./src/assets/postcss/styles.css')
+        .pipe(
+            postcss([
+                cssImport,
+                cssvars,
+                mixins,
+                nested,
+                pixelsToRem,
+                colorFunctions,
+                cssNext
+            ])
+        )
+        .on('error', errorInfo)
+        .pipe(cssnano())
+        .pipe(gulp.dest('./src/assets/css/'));
+});
+
+gulp.task('watch', function () {
+
+    gulp.watch('./src/assets/postcss/**/*.css', function () {
+        gulp.start('cssInject');
+    });
+});
+
+gulp.task('cssInject', ['styles'], function () {
+    return gulp.src('./src/assets/css/styles.css')
+        .pipe(cssnano())
+});
+
+function errorInfo(err) {
+    console.log(err);
+    this.emit('end');
+}
---
+++ src/shared/Header.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <h1>I am the header component</h1>
+    )
+}
